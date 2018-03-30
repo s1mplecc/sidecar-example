f85bf808392cd635524fb3bda471b3c96f841a64
s1mple <798342418@qq.com>
2018-03-30 09:56:47 +0800

    #sidecar-example - zhangzhen - 添加 Author-Service

---
+++ author-service/src/main/java/info/s1mple/author/AuthorController.java
@@ -0,0 +1,7 @@
+package info.s1mple.author;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorController {
+}
---
+++ author-service/src/main/java/info/s1mple/author/AuthorServiceApplication.java
@@ -0,0 +1,14 @@
+package info.s1mple.author;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorServiceApplication.class, args);
+    }
+}
