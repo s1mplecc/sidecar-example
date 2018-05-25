296cd171927d7a5709e62fd01cbe8e3473703b0e
s1mple <798342418@qq.com>
2018-05-25 10:35:00 +0800

    #sidecar-example - zhangzhen - 将 config-server 移动到包内

--- config-server/src/main/java/info/s1mple/config/ConfigApplication.java
+++ config-server/src/main/java/info/s1mple/config/ConfigApplication.java
@@ -1,3 +1,5 @@
+package info.s1mple.config;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
